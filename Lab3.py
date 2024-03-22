dccbe677fc0c3d5672fce80eddad8be46a0a3cba
Semiys <97906094+Semiys@users.noreply.github.com>
2024-03-22 17:30:04 +0400

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -1,70 +1,109 @@
-# Вариант 25. Формируется матрица F следующим образом: если в Е количество нулей в нечетных столбцах в области 1 больше, 
-# чем произведение чисел по периметру области 2, то поменять в В симметрично области 1 и 3 местами, 
-# иначе С и Е поменять местами несимметрично. При этом матрица А не меняется. 
-# После чего вычисляется выражение: ((К*A T)*(F+А)-K* F T . 
+# Вариант 25. Формируется матрица F следующим образом: если в Е количество нулей в нечетных столбцах в области 1 больше,
+# чем произведение чисел по периметру области 2, то поменять в В симметрично области 1 и 3 местами,
+# иначе С и Е поменять местами несимметрично. При этом матрица А не меняется.
+# После чего вычисляется выражение: ((К*A T)*(F+А)-K* F T .
 # Выводятся по мере формирования А, F и все матричные операции последовательно.
+import random
+
+# Функция для создания матрицы с целенаправленным заполнением для тестирования
+def create_test_matrix(N):
+    return [[random.randint(-10, 10) for _ in range(N)] for _ in range(N)]
+
+# Функция для транспонирования матрицы
+def transpose(matrix):
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+# Функция для сложения матриц
+def add_matrices(A, B):
+    return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+
+# Функция для умножения матриц
+def multiply_matrices(A, B):
+    result = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+
+# Функция для умножения матрицы на число
+def multiply_matrix_by_constant(K, matrix):
+    return [[K * matrix[i][j] for j in range(len(matrix[0]))] for i in range(len(matrix))]
+
+# Функция для подсчета количества нулей в нечетных столбцах левого треугольника подматрицы E
+def count_zeros_in_odd_columns_of_left_triangle(matrix):
+    count = 0
+    for i in range(len(matrix)):
+        for j in range(i + 1):
+            if j % 2 == 0 and matrix[i][j] == 0:
+                count += 1
+    return count
+
+# Функция для вычисления произведения чисел по периметру верхнего треугольника подматрицы E
+def product_of_perimeter_of_upper_triangle(matrix):
+    size = len(matrix)
+    product = 1
+    for i in range(size):
+        for j in range(size - i):
+            if i == 0 or j == 0 or j == size - i - 1:
+                product *= matrix[i][j]
+    return product
+
+# Функция для формирования матрицы F
+def create_matrix_F(A, B, C, D, E):
+    F = [row[:] for row in A]  # Копирование матрицы A
+    zeros_in_E1 = count_zeros_in_odd_columns_of_left_triangle(E)
+    product_of_E2_perimeter = product_of_perimeter_of_upper_triangle(E)
+
+    if zeros_in_E1 > product_of_E2_perimeter:
+        
+        for i in range(N // 2):
+            for j in range(i + 1):
+                F[i][j], F[N // 2 + i][N - j - 1] = F[N // 2 + i][N - j - 1], F[i][j]
+    else:
+        
+        for i in range(N // 2):
+            for j in range(N // 2):
+                F[i][j + N // 2], F[i + N // 2][j] = E[i][j], C[i][j]
+
+    return F
+
+
+# Вывод матрицы
 def print_matrix(matrix):
     for row in matrix:
         print(' '.join(map(str, row)))
+    print()
 
-def fill_matrix(N):
-    matrix = [[0]*N for _ in range(N)]
-    half = N // 2
-    for i in range(N):
-        for j in range(N):
-            if i < half and j < half:
-                matrix[i][j] = 1
-            elif i < half and j >= half:
-                matrix[i][j] = 2
-            elif i >= half and j < half:
-                matrix[i][j] = 3
-            else:
-                matrix[i][j] = 4
-    return matrix
 
-def create_matrix_F(A, N):
-    F = [[0]*N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            if j % 2 == 1 and A[i][j] == 0:
-                F[i][j] = A[i][j] + 3
-            else:
-                F[i][j] = A[i][j] - 1
-    return F
-
-def transpose_matrix(matrix):
-    return [list(row) for row in zip(*matrix)]
-
-def add_matrices(matrix1, matrix2):
-    return [[matrix1[i][j] + matrix2[i][j] for j in range(len(matrix1[0]))] for i in range(len(matrix1))]
-
-def multiply_matrix_by_number(matrix, number):
-    return [[number * matrix[i][j] for j in range(len(matrix[0]))] for i in range(len(matrix))]
-
-def multiply_matrices(matrix1, matrix2):
-    return [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*matrix2)] for X_row in matrix1]
-
-N = int(input("Введите размер матрицы N: "))
 K = int(input("Введите число K: "))
+N = int(input("Введите размерность N: "))
 
-A = fill_matrix(N)
-F = create_matrix_F(A, N)
 
-A_T = transpose_matrix(A)
-F_T = transpose_matrix(F)
 
-K_A_T = multiply_matrix_by_number(A_T, K)
-F_plus_A = add_matrices(F, A)
-K_F_T = multiply_matrix_by_number(F_T, K)
-
-temp_result = multiply_matrices(K_A_T, F_plus_A)
-final_result = add_matrices(temp_result, multiply_matrix_by_number(K_F_T, -1))
-
+A = create_test_matrix(N)
 print("Матрица A:")
 print_matrix(A)
 
-print("\nМатрица F:")
+
+half = N // 2
+B = [row[:half] for row in A[:half]]
+C = [row[half:] for row in A[:half]]
+D = [row[:half] for row in A[half:]]
+E = [row[half:] for row in A[half:]]
+
+
+F = create_matrix_F(A, B, C, D, E)
+print("Матрица F:")
 print_matrix(F)
 
-print("\nРезультат выражения ((K*A^T)*(F+A)-K*F^T):")
-print_matrix(final_result)
+
+A_T = transpose(A)
+F_T = transpose(F)
+F_plus_A = add_matrices(F, A)
+K_mult_A_T = multiply_matrix_by_constant(K, A_T)
+K_mult_F_T = multiply_matrix_by_constant(K, F_T)
+expr = add_matrices(multiply_matrices(K_mult_A_T, F_plus_A), K_mult_F_T)
+
+print("Результат выражения ((K * A.T) * (F + A)) - (K * F.T):")
+print_matrix(expr)
